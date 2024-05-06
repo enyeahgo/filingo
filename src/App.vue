625a052e6be790f851aa6d618737fba853c0c8da
Inigo Orosco <enyeahgo@gmail.com>
2024-05-06 15:17:19 +0800

    New Commit

--- src/App.vue
+++ src/App.vue
@@ -3,32 +3,36 @@
     <h1 class="text-xl font-bold">Filingo</h1>
   
     <nav class="hidden lg:flex space-x-4">
-      <a href="#" class="hover:text-gray-400">Home</a>
-      <a href="#" class="hover:text-gray-400">About</a>
-      <a href="#" class="hover:text-gray-400">Services</a>
-      <a href="#" class="hover:text-gray-400">Contact</a>
+      <router-link to="/" class="hover:text-gray-400">Home</router-link>
+      <router-link to="/about" class="hover:text-gray-400">About</router-link>
+      <router-link to="/services" class="hover:text-gray-400">Services</router-link>
+      <router-link to="/contact" class="hover:text-gray-400">Contact</router-link>
     </nav>
   
     <button id="menu-btn" class="lg:hidden focus:outline-none" @click="showMenu">
       <svg class="h-6 w-6 fill-current text-white" viewBox="0 0 24 24" ref="burger">
         <path fill-rule="evenodd" d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z" clip-rule="evenodd" />
       </svg>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20" fill="white" class="hidden" ref="cross"><g id="_01_align_center" data-name="01 align center"><polygon points="24 1.414 22.586 0 12 10.586 1.414 0 0 1.414 10.586 12 0 22.586 1.414 24 12 13.414 22.586 24 24 22.586 13.414 12 24 1.414"/></g></svg>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="15" height="15" stroke="white" fill="white" class="hidden mr-1" ref="cross"><g id="_01_align_center" data-name="01 align left"><polygon points="24 1.414 22.586 0 12 10.586 1.414 0 0 1.414 10.586 12 0 22.586 1.414 24 12 13.414 22.586 24 24 22.586 13.414 12 24 1.414"/></g></svg>
     </button>
   </header>
   <div class="hidden flex space-x-4" ref="menu">
     <div class="fixed top-0 left-0 h-screen w-64 bg-gray-800 text-white shadow-lg overflow-y-auto">
       <h2 class="text-xl font-bold px-4 py-2">Filingo</h2>
       <ul class="px-4 py-2">
-        <li><a href="#" class="hover:text-gray-400 block px-4 py-2">Home</a></li>
-        <li><a href="#" class="hover:text-gray-400 block px-4 py-2">About</a></li>
-        <li><a href="#" class="hover:text-gray-400 block px-4 py-2">Services</a></li>
-        <li><a href="#" class="hover:text-gray-400 block px-4 py-2">Contact</a></li>
+        <li><router-link to="/" class="hover:text-gray-400 block px-4 py-2" @click="showMenu">Home</router-link></li>
+        <li><router-link to="/about" class="hover:text-gray-400 block px-4 py-2" @click="showMenu">About</router-link></li>
+        <li><router-link to="/services" class="hover:text-gray-400 block px-4 py-2" @click="showMenu">Services</router-link></li>
+        <li><router-link to="/contact" class="hover:text-gray-400 block px-4 py-2" @click="showMenu">Contact</router-link></li>
       </ul>
     </div>
-    <div class="flex-grow px-4 py-4">
-      </div>
+    <div class="flex-grow px-4 py-4"></div>
   </div>
+  
+  <div class="mt-2 lg:mt-4">
+    <router-view/>
+  </div>
+  
 </template>
 
 <script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <h1>Hello, welcome to Filingo</h1>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'HomeView',
+  components: { }
+}
+</script>
